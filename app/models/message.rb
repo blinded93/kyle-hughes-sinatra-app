6a48451f293cee7673e645d94da2c4e117ee301f
blinded93 <blinded93@gmail.com>
2017-07-08 11:33:20 -0700

    added helper methods to the message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,15 @@
 class Message <ActiveRecord::Base
   belongs_to :user
 
+  def sender
+    User.find(self.sender_id)
+  end
+
+  def recipe
+    Recipe.find(self.recipe_id)
+  end
+
+  def recipe_name_short
+    self.recipe.name.split.first(5).join(" ") + "..."
+  end
 end
