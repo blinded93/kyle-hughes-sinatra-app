a9a4877e6c56e4d131c68c8b192fb3f24dbfc126
blinded93 <blinded93@gmail.com>
2017-07-08 11:46:10 -0700

    changed friend dropdown menu values to id for ease of tracking and personal message box from input to textarea.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706201505) do
+ActiveRecord::Schema.define(version: 20170708025211) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 20170706201505) do
     t.integer "owner_id"
     t.integer "sender_id"
     t.string "content"
+    t.integer "recipe_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "recipes", force: :cascade do |t|
